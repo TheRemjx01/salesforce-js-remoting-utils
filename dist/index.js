6dc414aa9e39079b7ae72b7ef9f1ed3251552d40
TheRemjx01 <remjx.is.me@gmail.com>
2017-12-18 15:04:21 +0700

    Final version of v0.0.2

--- dist/index.js
+++ dist/index.js
@@ -2,12 +2,15 @@
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-function validateRemotingApi(remoteApi) {
-    for (var key in remoteApi) {
-        if (!remoteApi.hasOwnProperty(key)) {
-            return 'Missing Any Api Key is prohibited';
+function validateRemotingApi(remoteApi, isSingleApi) {
+    if (!isSingleApi) {
+        for (var key in remoteApi) {
+            if (!remoteApi.hasOwnProperty(key)) {
+                return 'Missing Any Api Key is prohibited';
+            }
         }
     }
+
     if (!remoteApi.callback || !remoteApi.callback instanceof Function) {
         return 'Callback is not instance of function';
     }
@@ -15,26 +18,17 @@ function validateRemotingApi(remoteApi) {
 exports.generateAllApis = function (allRemotingApis) {
     return allRemotingApis.reduce(function (apis, remoteApi) {
         // check empty
-        var err = validateRemotingApi(remoteApi);
-        if (err) {
-            throw err;
-        }
-
-        // check callback is function
-        // const {jsRemoteMethod, sfController, callback, outputMethod} = remoteApi;
-        // const remoteCallName = `${sfController}.${jsRemoteMethod}`;
-
-        var _generateApi = generateApi(remoteApi),
-            outputMethod = _generateApi.outputMethod,
-            outputFunc = _generateApi.outputFunc;
+        var _generateApiPrivate = generateApiPrivate(remoteApi),
+            outputMethod = _generateApiPrivate.outputMethod,
+            outputFunc = _generateApiPrivate.outputFunc;
 
         apis[outputMethod] = outputFunc;
         return apis;
     }, {});
 };
 
-exports.generateApi = function (remoteApi) {
-    var err = validateRemotingApi(remoteApi);
+function generateApiPrivate(remoteApi) {
+    var err = validateRemotingApi(remoteApi, false);
     if (err) {
         throw err;
     }
@@ -53,5 +47,23 @@ exports.generateApi = function (remoteApi) {
     return {
         outputMethod: outputMethod,
         outputFunc: outputFunc
+    };
+}
+
+exports.generateApi = function (remoteApi) {
+    var err = validateRemotingApi(remoteApi, true);
+    if (err) {
+        throw err;
+    }
+    var jsRemoteMethod = remoteApi.jsRemoteMethod,
+        sfController = remoteApi.sfController,
+        callback = remoteApi.callback;
+
+    var remoteCallName = sfController + '.' + jsRemoteMethod;
+    return function (allParams) {
+        var _Visualforce$remoting2;
+
+        var spreadParams = [].concat(Array.prototype.slice.call(arguments));
+        (_Visualforce$remoting2 = Visualforce.remoting.Manager).invokeAction.apply(_Visualforce$remoting2, [remoteCallName].concat(_toConsumableArray(spreadParams), [callback, { escape: false }]));
     };
 };
--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
-function validateRemotingApi(remoteApi) {
-    for(let key in remoteApi) {
-        if (!remoteApi.hasOwnProperty(key)) {
-            return 'Missing Any Api Key is prohibited'
+function validateRemotingApi(remoteApi, isSingleApi) {
+    if(!isSingleApi) {
+        for(let key in remoteApi) {
+            if (!remoteApi.hasOwnProperty(key)) {
+                return 'Missing Any Api Key is prohibited'
+            }
         }
     }
+
     if (!remoteApi.callback || !remoteApi.callback instanceof Function) {
         return 'Callback is not instance of function'
     }
 }
 exports.generateAllApis = function (allRemotingApis) {
     return allRemotingApis.reduce((apis, remoteApi) => {
-        // check empty
-        const err = validateRemotingApi(remoteApi);
-        if (err) {
-            throw err
-        }
-
-    // check callback is function
-    // const {jsRemoteMethod, sfController, callback, outputMethod} = remoteApi;
-    // const remoteCallName = `${sfController}.${jsRemoteMethod}`;
-    const {outputMethod, outputFunc} = generateApi(remoteApi);
+    // check empty
+    const {outputMethod, outputFunc} = generateApiPrivate(remoteApi);
     apis[outputMethod] = outputFunc;
     return apis;
 }, {})
 };
 
-exports.generateApi = function(remoteApi) {
-    const err = validateRemotingApi(remoteApi);
+ function generateApiPrivate(remoteApi) {
+    const err = validateRemotingApi(remoteApi, false);
     if (err) {
         throw err
     }
@@ -43,4 +38,20 @@ exports.generateApi = function(remoteApi) {
       outputMethod,
       outputFunc
     }
+}
+
+exports.generateApi= function(remoteApi) {
+    const err = validateRemotingApi(remoteApi, true);
+    if (err) {
+        throw err
+    }
+    const {jsRemoteMethod, sfController, callback} = remoteApi;
+    const remoteCallName = `${sfController}.${jsRemoteMethod}`;
+    return function(allParams) {
+        const spreadParams = [...arguments];
+        Visualforce.remoting.Manager.invokeAction(
+            remoteCallName,
+            ...spreadParams, callback, {escape: false}
+        );
+    };
 };
